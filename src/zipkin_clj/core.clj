61dfb5e3d7b44e83d95126dfacd139475bd525c5
Simon Skorokhodov <skorohodovsemen@gmail.com>
2017-09-19 15:28:29 +0200

    fix updating current span from different threads

--- src/zipkin_clj/core.clj
+++ src/zipkin_clj/core.clj
@@ -1,11 +1,21 @@
-(ns zipkin-clj.core)
+(ns zipkin-clj.core
+  "Before tracing you should set a sender fn passing it to set-sender!.
+  The default sender does nothing.
+
+  IMPORTANT:
+  Start time is captured in microseconds as System/currentTimeMillis * 1000.
+  Duration time is measured in microseconds as a difference between
+  System/nanoTime devided by 1000.")
 
 ;; ====================================================================
 ;; Internal
 
-(def ^:private *sender (atom (fn [_])))
+ ; TODO possibly it should print something
+(defn- default-sender [_])
 
-(def ^:private ^:dynamic *current-span* nil)
+(def ^:private *sender (atom default-sender))
+
+(def ^:dynamic *current-span* nil)
 
 (defn- start-time []
   (System/nanoTime))
@@ -29,7 +39,7 @@
 
 (defn- opt-update-span!  [f]
   (if-let [span *current-span*]
-    (set! *current-span* (f span))))
+    (swap! span f)))
 
 ;; ====================================================================
 ;; API
@@ -37,50 +47,6 @@
 (defn set-sender!
   [sender]
   (reset! *sender sender))
-
-(defn start-span
-  [{span-name :span
-    service :service
-    parent :parent
-    annotations :annotations
-    tags :tags}]
-  (let [timestamp (current-time-us)]
-    (merge
-      {::start (start-time)
-       :id (id64)
-       :name span-name
-       :timestamp timestamp
-       :annotations
-       (map #(annotation timestamp %) annotations)
-       :tags (or tags {})}
-      (if-let [{:keys [traceId localEndpoint id]} (or parent *current-span*)]
-        {:traceId traceId
-         :localEndpoint localEndpoint
-         :parentId id}
-        (merge
-          {:traceId (id64)}
-          (if service
-            {:localEndpoint {:serviceName service}}))))))
-
-(defn finish-span
-  [span]
-  (let [us (duration-us (::start span))
-        span (-> span (assoc :duration us) (dissoc ::start))]
-    (@*sender [span])))
-
-(defn trace!*
-  [opts f]
-  (let [span (start-span opts)]
-    (binding [*current-span* span]
-      (try
-        (f)
-        (finally
-          (let [span *current-span*]
-            (finish-span span)))))))
-
-(defmacro trace!
-  [opts & body]
-  `(trace!* ~opts #(do ~@body)))
 
 (defn tag
   [span tags]
@@ -101,4 +67,44 @@
   (opt-update-span! #(apply annotate % annotations)))
 
 (defn current-span []
-  *current-span*)
+  (if-let [span *current-span*]
+    @span))
+
+(defn start-span
+  [{span-name :span
+    service :service
+    parent :parent
+    annotations :annotations
+    tags :tags
+    :as opts}]
+  (let [timestamp (current-time-us)]
+    (merge
+      {::start (start-time)
+       :id (id64)
+       :name span-name
+       :timestamp timestamp
+       :annotations (map #(annotation timestamp %) annotations)
+       :tags (or tags {})}
+      (if-let [{:keys [traceId localEndpoint id]} (if (contains? opts :parent)
+                                                    parent
+                                                    (current-span))]
+        {:traceId traceId
+         :localEndpoint localEndpoint
+         :parentId id}
+        {:traceId (id64)})
+      (if service
+        {:localEndpoint {:serviceName service}}))))
+
+(defn finish-span
+  [span]
+  (let [us (duration-us (::start span))
+        span (-> span (assoc :duration us) (dissoc ::start))]
+    (@*sender [span])))
+
+(defmacro trace!
+  [opts & body]
+  `(binding [*current-span* (atom (start-span ~opts))]
+     (try
+       ~@body
+       (finally
+         (finish-span @*current-span*)))))
