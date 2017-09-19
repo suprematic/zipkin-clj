4f4e0dbab25f7a680cd1db8b96deed9a41f8dbe3
Alexey Aristov <aav@acm.org>
2017-09-19 18:06:37 +0200

    build and maven deploy to private repo

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject zipkin-clj "0.1.0-SNAPSHOT"
+  :license "Proprietary closed source"
+  :url "http://www.suprematic.net"
   :description "Zipkin instrumentation library for Clojure."
-  :url "http://github.com/suprematic/zipkin-clj"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :plugins [[lein-ancient "0.6.10"]
+            [s3-wagon-private "1.3.0"]]
+
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
   :profiles {:test {:dependencies [[org.clojure/core.match "0.3.0-alpha5"]]}
-             :uberjar {:aot :all}})
+             :uberjar {:aot :all}}
+
+  :repositories [["snapshots" {:url "s3p://suprematic/mvn/snapshots/" :no-auth true}]
+                 ["releases"  {:url "s3p://suprematic/mvn/releases/"  :no-auth true}]])
