1a1551e3d581fbcbf427b76af4d0c767b333b77c
Simon Skorokhodov <skorohodovsemen@gmail.com>
2017-11-28 13:46:39 +0100

    pipeline build for release-* tags

--- project.clj
+++ project.clj
@@ -11,5 +11,8 @@
   :profiles {:test {:dependencies [[org.clojure/core.match "0.3.0-alpha5"]]}
              :uberjar {:aot :all}}
 
-  :repositories [["snapshots" {:url "s3p://suprematic/mvn/snapshots/" :no-auth true}]
-                 ["releases"  {:url "s3p://suprematic/mvn/releases/"  :no-auth true}]])
+  :repositories [["snapshots" {:url "s3p://suprematic/mvn/snapshots/"
+                               :no-auth true}]
+                 ["releases" {:url "s3p://suprematic/mvn/releases/"
+                              :no-auth true
+                              :sign-releases false}]])
