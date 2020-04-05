b109d3f4ea8c5c1440cf1e973fa635a77ff0c464
Simon Skorokhodov <skorohodovsemen@gmail.com>
2020-04-05 17:34:36 +0200

    project: change project's url to github.com

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject zipkin-clj "0.1.1-SNAPSHOT"
-  :url "http://www.suprematic.net"
   :description "Zipkin instrumentation library for Clojure."
+  :url "https://github.com/suprematic/zipkin-clj"
   :license
   {:name "Eclipse Public License - v1.0"
    :url  "https://www.eclipse.org/legal/epl-v10.html"}
