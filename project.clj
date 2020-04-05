dea18486d1ae180d734bcccb2914331935ebf11d
Simon Skorokhodov <skorohodovsemen@gmail.com>
2020-04-05 18:19:30 +0200

    project: remove private maven repos from project.clj

--- project.clj
+++ project.clj
@@ -7,10 +7,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
   :profiles {:test {:dependencies [[org.clojure/core.match "0.3.0-alpha5"]]}
-             :uberjar {:aot :all}}
+             :uberjar {:aot :all}})
 
-  :repositories [["snapshots" {:url "s3p://suprematic/mvn/snapshots/"
-                               :no-auth true}]
-                 ["releases" {:url "s3p://suprematic/mvn/releases/"
-                              :no-auth true
-                              :sign-releases false}]])
