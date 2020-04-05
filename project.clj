2fa7c3f366c71778c06654f52b2872cfdbfc5838
Simon Skorokhodov <skorohodovsemen@gmail.com>
2020-04-05 17:33:45 +0200

    license: switch to EPL v1

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject zipkin-clj "0.1.1-SNAPSHOT"
-  :license "Proprietary closed source"
   :url "http://www.suprematic.net"
   :description "Zipkin instrumentation library for Clojure."
+  :license
+  {:name "Eclipse Public License - v1.0"
+   :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
 
   :dependencies [[org.clojure/clojure "1.8.0"]]
