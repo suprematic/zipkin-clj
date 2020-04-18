c2c112e3b43d31900b51d7cad081fb66fd5a0fb4
Simon Skorokhodov <skorohodovsemen@gmail.com>
2020-04-18 17:54:55 +0200

    bump version to 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zipkin-clj "0.4.0"
+(defproject zipkin-clj "0.4.1-SNAPSHOT"
   :description "Zipkin instrumentation library for Clojure."
   :url "https://github.com/suprematic/zipkin-clj"
   :license
